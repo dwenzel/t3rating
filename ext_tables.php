f04553dcb2f059f883900facdae2e9a554b56575
Dirk Wenzel <wenzel@cps-it.de>
2014-12-30 14:03:03 +0100

    [TASK] code cleanup

--- ext_tables.php
+++ ext_tables.php
@@ -22,7 +22,6 @@ if (!defined('TYPO3_MODE')) {
 );
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Rating');
-
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addLLrefForTCAdescr('tx_t3rating_domain_model_voting', 'EXT:t3rating/Resources/Private/Language/locallang_csh_tx_t3rating_domain_model_voting.xlf');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_t3rating_domain_model_voting');
 $TCA['tx_t3rating_domain_model_voting'] = array(
@@ -111,40 +110,4 @@ $TCA['tx_t3rating_domain_model_vote'] = array(
 	),
 );
 
-/*
-$tmp_t3rating_columns = array(
-
-	'fe_user' => array(
-		'exclude' => 0,
-		'label' => 'LLL:EXT:t3rating/Resources/Private/Language/locallang_db.xlf:tx_t3rating_domain_model_collection.fe_user',
-		'config' => array(
-			'type' => 'select',
-			'foreign_table' => 'fe_users',
-			'minitems' => 0,
-			'maxitems' => 1,
-		),
-	),
-);
-
-$tmp_t3rating_columns['choice'] = array(
-	'config' => array(
-		'type' => 'passthrough',
-	)
-);
-
-t3lib_extMgm::addTCAcolumns('sys_file_collection',$tmp_t3rating_columns);
-
-$TCA['sys_file_collection']['columns'][$TCA['sys_file_collection']['ctrl']['type']]['config']['items'][] = array('LLL:EXT:t3rating/Resources/Private/Language/locallang_db.xlf:sys_file_collection.tx_extbase_type.Tx_T3rating_Collection','Tx_T3rating_Collection');
-
-$TCA['sys_file_collection']['types']['Tx_T3rating_Collection']['showitem'] = $TCA['sys_file_collection']['types']['1']['showitem'];
-$TCA['sys_file_collection']['types']['Tx_T3rating_Collection']['showitem'] .= ',--div--;LLL:EXT:t3rating/Resources/Private/Language/locallang_db.xlf:tx_t3rating_domain_model_collection,';
-$TCA['sys_file_collection']['types']['Tx_T3rating_Collection']['showitem'] .= 'fe_user';
-*/
-
-
-## EXTENSION BUILDER DEFAULTS END TOKEN - Everything BEFORE this line is overwritten with the defaults of the extension builder
-// @FIXME dw 2013-09-20: generated config above fails  with DB type mismatch. 
-//$TCA['sys_file_collection']['types']['static']['showitem'] .= ',--div--;LLL:EXT:t3rating/Resources/Private/Language/locallang_db.xlf:tx_t3rating_domain_model_collection,';
-//$TCA['sys_file_collection']['types']['static']['showitem'] .= 'fe_user';
-?>
 
